b20bbef90526e784c428f1da48616a25808663e6
olympai <153024858+olympai@users.noreply.github.com>
2024-11-10 10:44:18 +0100

    Update db_tables.py

--- db_config/db_tables.py
+++ db_config/db_tables.py
@@ -18,9 +18,21 @@ class User(db.Model, UserMixin):
 # table for the preferences
 class Preferences(db.Model):
     __tablename__ = 'preferences'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    ...
-    # add some parameters and keys
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    pets = db.Column(db.Boolean, nullable=True) # True if animals are no problem
+    smoking = db.Column(db.Boolean, nullable=True)
+    sex = db.Column(db.Boolean, nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    relationship_status = db.Column(db.Boolean, nullable=True)
+    degree = db.Column(db.Boolean, nullable=True) # True for Master, False for Bachelor
+    parking_spots = db.Column(db.Boolean, nullable=True)
+    language = db.Column(db.String(20), nullable=True)
+    condition = db.Column(db.Boolean, nullable=True) # True for new, False for old
+    community = db.Column(db.Integer, nullable=True) # Scale from 1 to 5
+    attendance = db.Column(db.Boolean, nullable=True) # True for weekend there, False for weekend away
+    semester = db.Column(db.Integer, nullable=True)
+    fitness = db.Column(db.Boolean, nullable=True)
+    baths_per_person = db.Column(db.Float, nullable=True)
 
 # tables for the matches
 class Matches(db.Model):
