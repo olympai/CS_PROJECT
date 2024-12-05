14b9aa08d2102a3ee308658f2cf3d6a1e85c72bd
Grégory Rickenmann <gregory.rickenmann@gmail.com>
2024-12-05 19:50:35 +0100

    added cross validation for ML and figured out optimal cluster amount

--- machine_learning/clustering.py
+++ machine_learning/clustering.py
@@ -5,10 +5,14 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
 from scipy.spatial.distance import euclidean
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.preprocessing import MinMaxScaler
+
+
 
 from db_config import db
 from db_config.db_tables import Matches, Offer
-from sklearn.model_selection import train_test_split, KFold
+
 
 # Initialize empty lists for matchings and matching scores
 matchings = []
@@ -25,11 +29,9 @@ def calculate_matching_score(df, user1, user2):
 def clustering_function(session_id):
     df = pd.read_sql('SELECT * FROM preferences', db.engine)
     df_later = df.copy()
-    print('1:', df[df['user_id'] == session_id])
     #replace NaNs with True to avoid errors
     df = df.fillna(True)
 
-    print('2:', df[df['user_id'] == session_id])
 
     #  Define features and hot-encode categorical variables
     X = pd.get_dummies(df[["pets", "sex", "age", "smoking"]])
@@ -40,31 +42,13 @@ def clustering_function(session_id):
     # Split the data into training and testing sets (5 fold cross validation)
     X_train, X_test = train_test_split(X, test_size=0.2, random_state=42)
 
-    #Standardize features (preprocessing to improve model performance)
-    scaler = StandardScaler() 
-    X = scaler.fit_transform(X) 
+    
+    # Standardize features using MinMaxScaler (preprocessing to improve model performance)
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
 
-    print('3:', df[df['user_id'] == session_id])
-
-    #define our clusters
-    #define cluster list, start from 2 so that silhouette score can be calculated
-    cluster_amount = list(range(10, 100))
-    best_score = -1
-    best_cluster = None
-    for i in cluster_amount:
-        kmeans = KMeans(n_clusters=i)
-        #fit the model 
-        df['cluster'] = kmeans.fit_predict(X)
-
-        #Evaluate performance using the silhouette score and find best performing cluster amount
-        silhouette_avg = silhouette_score(X, df['cluster'])
-        if silhouette_avg > best_score:
-            best_score = silhouette_avg
-            best_cluster = i
-
-    print('4:', df[df['user_id'] == session_id])
-
-    # print(f'Best Silhouette Score is: {best_score} for {best_cluster} clusters')
+    #define best amount of clusters based on clusterting.ipynb results
+    best_cluster = 11
 
     #fit the model for best amount of clusters
     kmeans = KMeans(n_clusters=best_cluster)
@@ -72,12 +56,8 @@ def clustering_function(session_id):
     #add the cluster to the dataframe
     df['cluster'] = kmeans.fit_predict(X)
 
-    print('5:', df[df['user_id'] == session_id])
-
     # Group customers by cluster
     clusters = df.groupby('cluster')['user_id'].apply(list).to_dict()
-
-    print('6:', clusters)
 
     # Add the matching customers to the matchings list
     for cluster, customers in clusters.items():
@@ -89,12 +69,8 @@ def clustering_function(session_id):
     # Convert matches to a DataFrame for better clarity
     matches_df = pd.DataFrame(matchings, columns=['user_id', 'offer_id'])
 
-    print('7:', matches_df[matches_df['user_id'] == session_id])
-
     # Add matching score to matches_df
     matches_df['matching_score'] = matches_df.apply(lambda row: calculate_matching_score(df_later, row['user_id'], row['offer_id']), axis=1)
-
-    print('8:', matches_df[matches_df['user_id'] == session_id])
 
     # Sort matches by matching score in ascending order (lower distance means better match)
     matches_df = matches_df.sort_values(by='matching_score')
@@ -102,11 +78,7 @@ def clustering_function(session_id):
     min_score = matches_df['matching_score'].min()
     max_score = matches_df['matching_score'].max()
 
-    print('9:', matches_df[matches_df['user_id'] == session_id])
-
     matches_df['normalized_matching_score'] = 100 * (1 - (matches_df['matching_score'] - min_score) / (max_score - min_score))
-
-    print('10:', matches_df[matches_df['user_id'] == session_id])
 
     # Print the DataFrame with normalized scores to check if it works
     print(matches_df)
