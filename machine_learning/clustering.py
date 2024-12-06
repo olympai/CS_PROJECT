f6f528df2a7f2506d898a8b104ecf812c996e37d
Grégory Rickenmann <gregory.rickenmann@gmail.com>
2024-12-06 10:58:03 +0100

    fixed logical inconsitencies and cleaned code

--- machine_learning/clustering.py
+++ machine_learning/clustering.py
@@ -19,7 +19,7 @@ matchings = []
 matching_score = 0
 
 #define matching score function
-# Calculate matching score based on euclidean distance
+# Calculate matching score based on euclidean distance, some back and forth conversion is needed to make sure the data is in the right format (np and dataframes). also NaNs are filled with 0 
 def calculate_matching_score(df, user1, user2):
     user1_preferences = df[df['user_id'] == user1].drop(['user_id'], axis=1).apply(pd.to_numeric, errors='coerce').fillna(0).values.flatten()
     user2_preferences = df[df['user_id'] == user2].drop(['user_id'], axis=1).apply(pd.to_numeric, errors='coerce').fillna(0).values.flatten()
@@ -28,13 +28,14 @@ def calculate_matching_score(df, user1, user2):
 #define clustering function
 def clustering_function(session_id):
     df = pd.read_sql('SELECT * FROM preferences', db.engine)
+    #used for matching scores later on
     df_later = df.copy()
     #replace NaNs with True to avoid errors
     df = df.fillna(True)
 
 
-    #  Define features and hot-encode categorical variables
-    X = pd.get_dummies(df[["age", "semester", "attendance", "fitness"]])
+    #  Define features as specified in clustering.ipynb
+    X = pd.get_dummies(df[["semester", "attendance", "fitness"]])
     #make sure true and false are replaced with 0 and 1
     X = np.where(X == True, 1, X)
     X = np.where(X == False, 0, X)
@@ -48,12 +49,12 @@ def clustering_function(session_id):
     X = scaler.fit_transform(X)
 
     #define best amount of clusters based on clusterting.ipynb results
-    best_cluster = 26
+    best_cluster = 10
 
     #fit the model for best amount of clusters
     kmeans = KMeans(n_clusters=best_cluster)
 
-    #add the cluster to the dataframe
+    #add the cluster to the dataframe (for matching)
     df['cluster'] = kmeans.fit_predict(X)
 
     # Group customers by cluster
@@ -63,7 +64,7 @@ def clustering_function(session_id):
     for cluster, customers in clusters.items():
         for i in range(len(customers)):
             for j in range(len(customers)):
-                if i != j:  # Avoid self-matching
+                if i != j:  # Avoid self-matching(!)
                     matchings.append((customers[i], customers[j]))
 
     # Convert matches to a DataFrame for better clarity
@@ -74,24 +75,25 @@ def clustering_function(session_id):
 
     # Sort matches by matching score in ascending order (lower distance means better match)
     matches_df = matches_df.sort_values(by='matching_score')
-    # Normalize matching scores to a range of 0-100
+    # Normalize matching scores to a range of 0-100 for better interpretation
     min_score = matches_df['matching_score'].min()
     max_score = matches_df['matching_score'].max()
-
     matches_df['normalized_matching_score'] = 100 * (1 - (matches_df['matching_score'] - min_score) / (max_score - min_score))
 
-    # Print the DataFrame with normalized scores to check if it works
-    print(matches_df)
-    print(session_id)
+    # # Print the DataFrame with normalized scores to check if it works
+    # print(matches_df)
+    # print(session_id)
 
     # Add matches to the database 
     this_df = matches_df[matches_df['user_id'] == session_id]
 
-    print(this_df)
+    # #another check if this works
+    # print(this_df)
 
     # delete old matches
     Matches.query.filter_by(user_id=session_id).delete()
     db.session.commit()
+
 
     for index, row in this_df.iterrows():
         print(int(row['offer_id']))
