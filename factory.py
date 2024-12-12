b1b078ab24ad2e62bdbdbdb262f22859e643c4a4
olympai <CaMuller@gmx.de>
2024-12-12 15:58:33 +0100

    changes

--- factory.py
+++ factory.py
@@ -13,12 +13,12 @@ from utils.utils import correct_uri
 def create_app():
     app = Flask(__name__)  
     # connect to the database
-    app.config['SQLALCHEMY_DATABASE_URI'] = correct_uri(os.environ.get('DATABASE_URL'))
+    # app.config['SQLALCHEMY_DATABASE_URI'] = correct_uri(os.environ.get('DATABASE_URL'))
     # for clarification and test: here the database URI is hardcoded
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u21fm5hco9rlre:p1708e307e0693a7c7b14f621f7250c7a6099a87641a1560cbb8daaa091333094@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d84um7ga402mvs'
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u21fm5hco9rlre:p1708e307e0693a7c7b14f621f7250c7a6099a87641a1560cbb8daaa091333094@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d84um7ga402mvs'
+    # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     # for clarification and test: here the secret key is hardcoded
-    # app.config['SECRET_KEY'] = 'ourcoolcsproject24'
+    app.config['SECRET_KEY'] = 'ourcoolcsproject24'
     
     db.init_app(app)
 
